03b560683ac10ff7cd340afb99edaeb55af1f8e2
YRustt <YRustt@yandex.ru>
2016-11-09 02:23:17 +0300

    -

--- iism_lab3.py
+++ iism_lab3.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from iism_lab2 import a5_generator
-
+from scipy.stats import chi2
 
 def get_a5_generator():
     S19 = np.random.randint(2, size=19)
@@ -10,20 +10,6 @@ def get_a5_generator():
     S23 = np.random.randint(2, size=23)
     gen = a5_generator(S19, S22, S23)
     return gen
-
-
-# def normal_psi(gen, depth):
-#     while True:
-#         n1, n2, n3 = 0, 1, 1
-#         m1, m2, m3 = 1, 2, 1
-#         for _ in range(depth):
-#             if next(gen) == 1:
-#                 n1, n2, n3 = n2, n2 + n3, n3
-#                 m1, m2, m3 = m2, m2 + m3, m3
-#             else:
-#                 n1, n2, n3 = n1, n1 + n2, n2
-#                 m1, m2, m3 = m1, m1 + m2, m2
-#         yield n2 / m2
 
 
 def uniform_psi(gen, n=10):
@@ -55,6 +41,72 @@ def exponential_psi(l=2):
     psi = uniform_psi(get_a5_generator(), n=10)
     while True:
         yield -np.log(next(psi)) / l
+
+
+def polynomial_psi(ps):
+    psi = uniform_psi(get_a5_generator(), n=10)
+    ps = np.cumsum(ps)
+    while True:
+        val = next(psi)
+        for i, p in enumerate(ps):
+            if val < p:
+                return i
+
+
+def M(res):
+    return np.mean(res)
+
+
+def D(res):
+    return np.std(res)
+
+
+def empirical_distribution_func(res):
+    ys, xs = np.histogram(res, bins=np.unique(res), density=True)
+    return xs, np.cumsum(ys)
+
+
+def empirical_histogram(res, xs):
+    _min, _max = xs[0], xs[-1]
+    ys = np.array([len([t for t in res if xs[i] <= t < xs[i + 1]]) for i in range(len(xs) - 1)]) / len(res)
+    return xs, ys
+
+
+def calc_function(func, xs, params=None):
+    ys = [func(x, **params) if params is not None else func(x) for x in xs]
+    return xs, ys
+
+
+def draw_empirical_distribution_func(res, func=None, params=None, ax=None, label=None, loc=None):
+    xs, ys = empirical_distribution_func(res)
+    ax.bar(xs[:-1], ys, label=label, color='green')
+    xs, ys = calc_function(func, xs[:-1], params)
+    ax.plot(xs, ys, color='blue')
+    ax.legend(loc=loc)
+
+
+def draw_empirical_histogram(res, xs, func=None, params=None, ax=None, label=None, loc=None):
+    xs, ys = empirical_histogram(res, xs)
+    ax.bar(xs[:-1], ys, label=label, color='green')
+    xs, ys = calc_function(func, xs[:-1], params)
+    ax.plot(xs, ys, color='blue')
+    ax.legend(loc=loc)
+
+
+def pearson_test(res, k, eps, func, *args):
+    _min, _max, n = min(res), max(res), len(res)
+    step = (_max - _min) / k
+    x = np.array([val for val in np.arange(_min, _max, step)])
+    y = np.array([len([t for t in res if val <= t < val + step]) for val in x]) / len(res)
+    f = list(map(lambda t: func(t, *args), x)) + [func(_max + step, *args)]
+    p = np.array(list(map(lambda x, y: x - y, f[:-1], f[1:])))
+    xsi = np.sum((y - n * p) ** 2 / (n * p))
+    P = 1 - chi2.cdf(xsi, k - 1)
+    return eps < P
+
+
+def kolmogorov_test(res, ):
+    pass
 
 
 if __name__ == '__main__':
